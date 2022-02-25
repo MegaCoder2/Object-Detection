34cc2aa70daebe124ba81c52b05cad642af2a2be
Mega <83192893+MegaCoder2@users.noreply.github.com>
2022-02-24 19:58:22 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
-var result
+var result = []
+var ml5status = ""
+var errordetect = false
+var i
 function bedroom()
 {
     window.location = "/test1.html"
@@ -30,7 +33,26 @@ function preload()
 }
 function setup()
 {
-    canvas = createCanvas(640, 420)
+    if(window.location.pathname == "/test1.html")
+    {
+        canvas = createCanvas(480, 404)
+    }
+    else if(window.location.pathname == "/test2.html")
+    {
+        canvas = createCanvas(850, 567)
+    }
+    else if(window.location.pathname == "/test3.html")
+    {
+        canvas = createCanvas(400, 400)
+    }
+    else if(window.location.pathname == "/test4.html")
+    {
+        canvas = createCanvas(1024, 512)
+    }
+    else if(window.location.pathname == "/test5.html")
+    {
+        canvas = createCanvas(350, 350)
+    }
     canvas.center()
     objectDetection = ml5.objectDetector("cocossd", modelLoaded)
     document.getElementById("status").innerHTML = "Status: Detecting Objects... (may take time)"
@@ -65,6 +87,7 @@ function gotResult(error, results)
     if(error)
     {
         console.log("An error occured:" + error)
+        errordetect = true
     }
     else
     {
@@ -76,22 +99,39 @@ function draw()
 {
     if(window.location.pathname == "/test1.html")
     {
-        image(img, 0, 0, 640, 420)
+        image(img, 0, 0, 480, 404)
     }
     else if(window.location.pathname == "/test2.html")
     {
-        image(img2, 0, 0, 640, 420)
+        image(img2, 0, 0, 850, 567)
     }
     else if(window.location.pathname == "/test3.html")
     {
-        image(img3, 0, 0, 640, 420)
+        image(img3, 0, 0, 400, 400)
     }
     else if(window.location.pathname == "/test4.html")
     {
-        image(img4, 0, 0, 640, 420)
+        image(img4, 0, 0, 1024, 512)
     }
     else if(window.location.pathname == "/test5.html")
     {
-        image(img5, 0, 0, 640, 420)
+        image(img5, 0, 0, 350, 350)
+    }
+    if(ml5status != "")
+    {
+        for(var i = 0; i < result.length; i++)
+        {
+            fill("#ff0000")
+            text(result[i].label + " (confidence: " + Math.round(result[i].confidence * 100) + "%)", result[i].x + 7.5, result[i].y + 15)
+            noFill()
+            stroke("#ff0000")
+            rect(result[i].x, result[i].y, result[i].width, result[i].height)
+            document.getElementById("status").innerHTML = "Status: Detecting Objects... (Detecting Objects " + Math.round(i + 1) + "/" + result.length + ")"
+        }
+        document.getElementById("status").innerHTML = "Detected " + result.length + " object(s)." 
+    }
+    if(errordetect == true)
+    {
+        document.getElementById("status").innerHTML = "An error occured, try again later."
     }
 }

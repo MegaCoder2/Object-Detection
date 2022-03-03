780af92be87e5efe0d12b9eed483dbc48853848a
Mega <83192893+MegaCoder2@users.noreply.github.com>
2022-03-02 18:40:02 -0600

    Update main.js

--- main.js
+++ main.js
@@ -4,23 +4,23 @@ var errordetect = false
 var i
 function bedroom()
 {
-    window.location = "/test1.html"
+    window.location = "Object-Detection/test1.html"
 }
 function tv()
 {
-    window.location = "/test2.html"
+    window.location = "Object-Detection/test2.html"
 }
 function desk()
 {
-    window.location = "/test3.html"
+    window.location = "Object-Detection/test3.html"
 }
 function bottles()
 {
-    window.location = "/test4.html"
+    window.location = "Object-Detection/test4.html"
 }
 function fruitbasket()
 {
-    window.location = "/test5.html"
+    window.location = "Object-Detection/test5.html"
 }
 img = ""
 function preload()
@@ -33,23 +33,23 @@ function preload()
 }
 function setup()
 {
-    if(window.location.pathname == "/test1.html")
+    if(window.location.pathname == "Object-Detection/test1.html")
     {
         canvas = createCanvas(480, 404)
     }
-    else if(window.location.pathname == "/test2.html")
+    else if(window.location.pathname == "Object-Detection/test2.html")
     {
         canvas = createCanvas(850, 567)
     }
-    else if(window.location.pathname == "/test3.html")
+    else if(window.location.pathname == "Object-Detection/test3.html")
     {
         canvas = createCanvas(400, 400)
     }
-    else if(window.location.pathname == "/test4.html")
+    else if(window.location.pathname == "Object-Detection/test4.html")
     {
         canvas = createCanvas(1024, 512)
     }
-    else if(window.location.pathname == "/test5.html")
+    else if(window.location.pathname == "Object-Detection/test5.html")
     {
         canvas = createCanvas(350, 350)
     }
@@ -61,23 +61,23 @@ function modelLoaded()
 {
     console.log("Model loaded successfully!")
     ml5status = true
-    if(window.location.pathname == "/test1.html")
+    if(window.location.pathname == "Object-Detection/test1.html")
     {
         objectDetection.detect(img, gotResult)
     }
-    else if(window.location.pathname == "/test2.html")
+    else if(window.location.pathname == "Object-Detection/test2.html")
     {
         objectDetection.detect(img2, gotResult)
     }
-    else if(window.location.pathname == "/test3.html")
+    else if(window.location.pathname == "Object-Detection/test3.html")
     {
         objectDetection.detect(img3, gotResult)
     }
-    else if(window.location.pathname == "/test4.html")
+    else if(window.location.pathname == "Object-Detection/test4.html")
     {
         objectDetection.detect(img4, gotResult)
     }
-    else if(window.location.pathname == "/test5.html")
+    else if(window.location.pathname == "Object-Detection/test5.html")
     {
         objectDetection.detect(img5, gotResult)
     }
@@ -97,23 +97,23 @@ function gotResult(error, results)
 }
 function draw()
 {
-    if(window.location.pathname == "/test1.html")
+    if(window.location.pathname == "Object-Detection/test1.html")
     {
         image(img, 0, 0, 480, 404)
     }
-    else if(window.location.pathname == "/test2.html")
+    else if(window.location.pathname == "Object-Detection/test2.html")
     {
         image(img2, 0, 0, 850, 567)
     }
-    else if(window.location.pathname == "/test3.html")
+    else if(window.location.pathname == "Object-Detection/test3.html")
     {
         image(img3, 0, 0, 400, 400)
     }
-    else if(window.location.pathname == "/test4.html")
+    else if(window.location.pathname == "Object-Detection/test4.html")
     {
         image(img4, 0, 0, 1024, 512)
     }
-    else if(window.location.pathname == "/test5.html")
+    else if(window.location.pathname == "Object-Detection/test5.html")
     {
         image(img5, 0, 0, 350, 350)
     }
